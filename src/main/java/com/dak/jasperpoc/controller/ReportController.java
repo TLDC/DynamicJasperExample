8d84897f8563ce6480e38351628ad8119f1f505d
David Key <davidkey@gmail.com>
2016-06-09 17:34:05 -0400

    well something is working

--- src/main/java/com/dak/jasperpoc/controller/ReportController.java
+++ src/main/java/com/dak/jasperpoc/controller/ReportController.java
@@ -1,13 +1,19 @@
 package com.dak.jasperpoc.controller;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import javax.transaction.Transactional;
 
+import org.hibernate.mapping.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +24,11 @@ import com.dak.jasperpoc.repository.EmployeeRepository;
 import com.dak.jasperpoc.service.ReportService;
 
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 @Controller
 @RequestMapping("/")
@@ -26,11 +36,13 @@ public class ReportController {
 
 	private final EmployeeRepository employeeRepository;
 	private final ReportService reportService;
+	private final DataSource dataSource;
 
 	@Autowired
-	public ReportController(final EmployeeRepository employeeRepository, final ReportService reportService){
+	public ReportController(final EmployeeRepository employeeRepository, final ReportService reportService, final DataSource dataSource){
 		this.employeeRepository = employeeRepository;
 		this.reportService = reportService;
+		this.dataSource = dataSource;
 	}
 
 	@RequestMapping(method = RequestMethod.GET)
@@ -39,11 +51,16 @@ public class ReportController {
 	}
 
 	@RequestMapping(value = "/employeeReport.pdf", method = RequestMethod.GET, produces = "application/pdf")
-	public void getEmployeeReportPdf(final HttpServletResponse response) throws JRException, IOException, ClassNotFoundException {
-		//EmployeeReport report = new EmployeeReport(employeeRepository.findAll());
-		//	JasperPrint jp = report.getReport();
-
-		JasperPrint jp = null;
+	public void getEmployeeReportPdf(final HttpServletResponse response) throws JRException, IOException, ClassNotFoundException, SQLException {
+		InputStream jasperStream = this.getClass().getResourceAsStream("/reports/Employees.jrxml");
+		JasperReport jasperReport = JasperCompileManager.compileReport(jasperStream);
+	//	IOUtils.closeQuietly(jasperStream);
+		
+		Map<String,Object> params = new HashMap<>();
+		JasperPrint jp = JasperFillManager.fillReport(jasperReport, params, new JRBeanCollectionDataSource(employeeRepository.findAll()));
+		
+		//JasperPrint jp = JasperFillManager.fillReport(jasperReport, params, new JRBeanCollectionDataSource(new ArrayList<>()));
+		//JasperPrint jp = JasperFillManager.fillReport(jasperReport, params, dataSource.getConnection());
 
 		reportService.writePdfReport(jp, response, "employeeReport");
 		return;
